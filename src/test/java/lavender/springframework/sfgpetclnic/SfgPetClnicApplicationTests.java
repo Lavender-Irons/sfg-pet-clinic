d32d3c7b9b3e174331a03f108a5046d704b681f5
Lavender <lavenderirons@gmail.com>
2019-07-27 17:50:34 -0500

    Initial Project Add

---
+++ src/test/java/lavender/springframework/sfgpetclnic/SfgPetClnicApplicationTests.java
@@ -0,0 +1,16 @@
+package lavender.springframework.sfgpetclnic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SfgPetClnicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
